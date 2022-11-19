d0c11c70ffdbd7a908d8faa0c709580fa20d82f5
Rober2092 <90662615+Rober2092@users.noreply.github.com>
2022-11-19 13:00:06 -0800

    edit components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/bd1e53f0-b104-44f2-ac1e-326e1b60f302" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/422e5f4e-ffe3-4756-a13f-1f39c1116c28" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>/ Submit the form below or shoot me an email -rober2091@icloud.com</p>
