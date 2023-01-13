28a3c91283f3c58723a105879a9f7e89a9a3e40c
Rober2092 <90662615+Rober2092@users.noreply.github.com>
2023-01-13 11:59:12 -0800

    add env file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
           Hugo Alvarado
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a Full Stack Developer.
+          I'm a Full Stack Web  Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
           I’m a full-stack web developer specializing in building (and occasionally
