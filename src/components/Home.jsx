04d5ac4bbfcd403655a6f3c629806f7f42ca6676
Rober2092 <90662615+Rober2092@users.noreply.github.com>
2023-10-24 15:33:53 -0700

    new code add to home botton

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
+  const workRef = useRef(null);
+
+  const scrollToWork = () => {
+    workRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
-      {/* Container */}
+      {/* Home Section */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hugo Alvarado</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Hugo Alvarado
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm  Full Stack Web  Developer.
+          I'm a Full Stack Web Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m full-stack web developer specializing in building (and occasionally
+        I’m full-stack web developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.
 
@@ -26,18 +32,31 @@ with HTML and CSS to make some minor edits on a small
 business website
 that I was operating. What I thought was just a few small edits turned into a love for programming.
 
-</p>
+          
+        </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+          <button
+            onClick={scrollToWork}
+            className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'
+          >
             View Work
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3 ' />
+            <span className='group-hover:rotate-90 duration-300 transform'>
+              <HiArrowNarrowRight className='ml-3' />
             </span>
           </button>
+        </div>
+      </div>
+      {/* Work Section */}
+      <div ref={workRef} className='w-full h-screen bg-[#1a2a3a]' id="work">
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+          <h1 className='text-4xl sm:text-7xl font-bold text-white'>
+          
+          </h1>
+          {/* ...other work section content... */}
         </div>
       </div>
     </div>
   );
 };
 
-export default Home;       
+export default Home;
